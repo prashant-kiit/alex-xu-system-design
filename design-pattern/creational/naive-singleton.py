eb620b4df8b6797d1503fad031712d7b1458b444
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-17 13:23:28 +0530

    Add Pythonic Singleton Implementation
    Enhanced the naive singleton implementation by introducing a decorator-based approach for creating singleton classes. Added a Logger class as an example, demonstrating shared state across instances. This update provides a more Pythonic way to implement the Singleton pattern, improving code readability and usability.

--- design-pattern/creational/naive-singleton.py
+++ design-pattern/creational/naive-singleton.py
@@ -20,3 +20,37 @@ if __name__ == "__main__":
     s1 = Singletone()
     s1 = Singletone()
     print(s1 is s1)
+
+
+"""
+More Pyhtonic Way of Singleton:
+
+def singleton(cls):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+
+@singleton
+class Logger:
+    def __init__(self):
+        self.logs = []
+
+    def log(self, message):
+        self.logs.append(message)
+        print(f"LOG: {message}")
+
+
+# Usage
+logger1 = Logger()
+logger2 = Logger()
+
+logger1.log("App started")
+print(logger1 is logger2)   # True
+print(logger2.logs)         # ["App started"] — shared state
+"""
